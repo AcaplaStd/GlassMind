f3532a0acf38fdcf0357305386369e698da12242
1Gregory <6squaress@gmail.com>
2019-02-27 21:54:32 +0300

    Fix

--- GlassMindFramework/NeuralNetwok.py
+++ GlassMindFramework/NeuralNetwok.py
@@ -1,14 +1,25 @@
 from GlassMindFramework.Neuron import Neuron
+
+
 class NeuralNetwork:
     """
     layers - (2, 4, 6, 8, 1) e.t.c.
     """
     def __init__(self, layers):
-        self.neurons = [[Neuron() for i in range(layers)] for layer in layers]
+        # self.activate_fnc = activate_fnc
+        # self.derivative = derivative
+
+        self.neurons = [[Neuron() for i in range(layer)] for layer in layers]
+
         last_layer = layers[0]  # Firstly, it will be
         self.weights = []
-        for layer in layers:
-
+        for layer in layers[1:]:
+            self.weights.append([])
+            for i in range(layer):
+                self.weights[-1].append([])
+                for ii in range(last_layer):
+                    self.weights[-1][-1].append(0)
+            last_layer = layer
 
         # self.weights = [[None for i in range(layer)] for layer in layers[1:]]
 
