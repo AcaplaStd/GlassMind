3ff1a9afc57a44909c1caab9d50d9ce70a9e7f63
Андреев Григорий <39461408+1Gregory@users.noreply.github.com>
2019-03-05 20:11:32 +0300

    Теперь используется html.html

--- GlassMind/app.py
+++ GlassMind/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_temlate
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("html.html")
 
 if __name__ == "__main__":
     app.run()
