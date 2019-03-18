289d39c349712b090fd895052c9081cf30d2bf6e
Ivan1874 <ivan51673@gmail.com>
2019-03-18 15:46:59 +0300

    Мдя. Червю это определенно понрвится. Вход готов, из дизайна только текстфилды. Пока времени нет

--- GlassMind/routes.py
+++ GlassMind/routes.py
@@ -1,10 +1,54 @@
-from flask import *
-from GlassMind import app
+from flask import render_template, send_from_directory, flash, redirect, url_for, request
+from . import app, db
+from .forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user
+from .models import User
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('some_page.html', is_whitemode=True)
+    return render_template('home_page.html', is_whitemode=True)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/static/css/<path:path>')
--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "6H'-=4@8LAXR2N5ON3+REZI&LRFYPG@D_LG1OB2J/A)C*5&A8ZF8\\#WM\\2#W$XCG"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgres://qnwpgnurknhiub:5c6d63f0e383521bf2d39a401ca5aa6542ee3083bfe916a8ad560e69d8ecb4a8@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/daqmle0p2g06lq"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
