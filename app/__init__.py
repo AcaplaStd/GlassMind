3a762a8b68b67daefa4865555c4a684725ce6ae9
Ivan1874 <ivan51673@gmail.com>
2019-04-13 22:09:00 +0300

    Maybe this? Now address starts with /gm

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.routing import Rule
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+app.url_rule_class = lambda path, **options: Rule(app.config['APPLICATION_ROOT'] + '/' + path, **options)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 from app import routes, models
