ea49b831c520b2609fab06d71cc94be1b82bff64
ivan1874 <ivan51673@gmail.com>
2019-04-13 18:04:33 +0000

    Gunicorngit add -A

--- GlassMind/__init__.py
+++ GlassMind/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +11,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 from GlassMind import routes, models
